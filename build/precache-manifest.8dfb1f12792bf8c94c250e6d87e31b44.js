93c8c4d87acbe69e513d1b2a5467cfef1e1b47de
aatma001 <70138225+aatma001@users.noreply.github.com>
2020-09-12 14:21:12 +0300

    frontti lisätty

---
+++ build/precache-manifest.8dfb1f12792bf8c94c250e6d87e31b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e662ab805be408479f77ac30b5fd2420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6abb5284b6fd9ecfbc",
+    "url": "/static/css/main.b224b90c.chunk.css"
+  },
+  {
+    "revision": "5b495e56d259e1add482",
+    "url": "/static/js/2.3793ae4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3793ae4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6abb5284b6fd9ecfbc",
+    "url": "/static/js/main.2f98ca37.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
