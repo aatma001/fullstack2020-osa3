7602439395b03b68cbc210359e792bc1be8a565c
aatma001 <70138225+aatma001@users.noreply.github.com>
2020-09-15 16:51:40 +0300

    lintattu

--- build/precache-manifest.8959c87f51002522291fafc46124e205.js
+++ build/precache-manifest.8959c87f51002522291fafc46124e205.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "363d81770b30f7a3383bf588d12b956b",
-    "url": "/index.html"
+    'revision': '363d81770b30f7a3383bf588d12b956b',
+    'url': '/index.html'
   },
   {
-    "revision": "5ef4fad02480c5f7752a",
-    "url": "/static/css/main.b224b90c.chunk.css"
+    'revision': '5ef4fad02480c5f7752a',
+    'url': '/static/css/main.b224b90c.chunk.css'
   },
   {
-    "revision": "5b495e56d259e1add482",
-    "url": "/static/js/2.3793ae4f.chunk.js"
+    'revision': '5b495e56d259e1add482',
+    'url': '/static/js/2.3793ae4f.chunk.js'
   },
   {
-    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.3793ae4f.chunk.js.LICENSE.txt"
+    'revision': 'e88a3e95b5364d46e95b35ae8c0dc27d',
+    'url': '/static/js/2.3793ae4f.chunk.js.LICENSE.txt'
   },
   {
-    "revision": "5ef4fad02480c5f7752a",
-    "url": "/static/js/main.f56912f0.chunk.js"
+    'revision': '5ef4fad02480c5f7752a',
+    'url': '/static/js/main.f56912f0.chunk.js'
   },
   {
-    "revision": "f0be2d6b237b3fb64219",
-    "url": "/static/js/runtime-main.24b8e7b7.js"
+    'revision': 'f0be2d6b237b3fb64219',
+    'url': '/static/js/runtime-main.24b8e7b7.js'
   }
-]);
+])
--- index.js
+++ index.js
@@ -1,132 +1,132 @@
-const express = require("express");
-var morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const Person = require("./models/person");
+const express = require('express')
+var morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
-app.use(express.static("build"));
-app.use(cors());
-app.use(express.json());
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
-morgan.token("person", (res) => {
-  return JSON.stringify(res.body);
-});
+morgan.token('person', (res) => {
+  return JSON.stringify(res.body)
+})
 
 app.use(
-  morgan("tiny", {
+  morgan('tiny', {
     skip: function (req, res) {
-      return req.method === "POST";
+      return req.method === 'POST'
     },
   })
-);
+)
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :person",
+    ':method :url :status :res[content-length] - :response-time ms :person',
     {
       skip: function (req, res) {
-        return req.method !== "POST";
+        return req.method !== 'POST'
       },
     }
   )
-);
+)
 
-app.get("/api/persons", (req, res) => {
-  console.log("etsitään");
+app.get('/api/persons', (req, res, next) => {
+  console.log('etsitään')
   Person.find({})
     .then((item) => {
-      res.json(item.map((person) => person.toJSON()));
+      res.json(item.map((person) => person.toJSON()))
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
-        res.json(person.toJSON());
+        res.json(person.toJSON())
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
-app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
   if (!body.name || !body.number) {
     return res.status(400).json({
-      error: "please enter name and a number",
-    });
+      error: 'please enter name and a number',
+    })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      res.json(savedPerson.toJSON());
+      res.json(savedPerson.toJSON())
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
-      res.status(204).end();
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body;
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.findByIdAndUpdate(req.params.id, person, {
     new: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedPerson) => {
-      res.json(updatedPerson.toJSON());
+      res.json(updatedPerson.toJSON())
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/info", (req, res, next) => {
-  console.log("info");
+app.get('/api/info', (req, res, next) => {
+  console.log('info')
   Person.countDocuments({})
     .then((count) => {
       res.send(
         `<p>Phonebook has info for ${count} people</p>
         <p>${new Date().toString()}`
-      );
+      )
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
-  if (error.name === "CastError" && error.kind === "ObjectId") {
-    return res.status(400).send({ error: "malformed id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
